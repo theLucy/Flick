164f2465e1bb192d90abecb04e5f60528f7c5e1b
root <root@lucy.lt>
2021-07-29 10:21:03 +0300

    multiple profiles improvement.

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 
+  profile_folder := "profiles/"
 
   go finger.Serve(finger.HandlerFunc(func(ctx context.Context, w io.Writer, q *finger.Query) {
     text := ""
@@ -20,25 +21,24 @@ func main() {
     if q.Username == "" {
       filename = "root"
     } else {
-      filename = q.Username
+      filename = profile_folder + q.Username
     }
 
     file, err := os.Open(filename)
     if err != nil {
-      text = "ERR: No such user!"
+      text = "ERR!\nNo such user!"
     }
     defer func() {
       if err = file.Close(); err != nil {
       }
     }()
 
-
     if err == nil {
       b, err := ioutil.ReadAll(file)
       if err != nil {
-        text = "ERR: No such user!"
+        text = "ERR!\nNo such user!"
       } else {
-        text = "MSG: " + string(b)
+        text = "OK!\n" + string(b)
       }
     }
 
